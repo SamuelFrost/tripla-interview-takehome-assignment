d8975e04b19e4fffe17c93010ab0e58526608ef8
Samuel Frost <samuelfurosuto@gmail.com>
2026-02-20 13:22:35 +0000

    add test for repeating error responses

--- dynamic-pricing/test/jobs/update_hotel_room_prices_job_test.rb
+++ dynamic-pricing/test/jobs/update_hotel_room_prices_job_test.rb
@@ -19,10 +19,28 @@ class UpdateHotelRoomPricesJobTest < ActiveJob::TestCase
   test "job retries on rate api error then succeeds on second attempt" do
     VCR.use_cassette("rate_api/get_pricing_all_attributes_retry_then_success") do
       assert_difference "HotelRoomPrice.count", TOTAL_SEASONAL_HOTEL_ROOM_COUNT do
-        perform_enqueued_jobs do
+        perform_enqueued_jobs only: UpdateHotelRoomPricesJob do
           UpdateHotelRoomPricesJob.perform_later
         end
       end
+      assert_performed_jobs 2, only: UpdateHotelRoomPricesJob
+      assert_equal performed_jobs.last["exception_executions"],  {"[RateApi::RateApiError, Faraday::TimeoutError, Faraday::ConnectionFailed]"=>1}
+    end
+  end
+
+  test "job does not retry indefinitely on rate api error" do
+    VCR.use_cassette("rate_api/get_pricing_all_attributes_500_error", allow_playback_repeats: true) do
+      begin
+        perform_enqueued_jobs only: UpdateHotelRoomPricesJob do
+          UpdateHotelRoomPricesJob.perform_later
+        end
+      rescue Minitest::UnexpectedError => test_failure_exception # as we are using a test adapter, minitest raises an UnexpectedError when the job raises an exception, not the job itself
+        error = test_failure_exception.error # extract the exception from the UnexpectedError
+      end
+      assert_instance_of RateApi::RateApiError, error, "Expected job to raise RateApi::RateApiError after exhausting retries"
+      assert_equal "Failed to get pricing, response status: 500", error.message, "Expected job to raise RateApi::RateApiError after exhausting retries"
+      assert_performed_jobs 3, only: UpdateHotelRoomPricesJob
+      assert_equal performed_jobs.last["exception_executions"],  {"[RateApi::RateApiError, Faraday::TimeoutError, Faraday::ConnectionFailed]"=>3}
     end
   end
 end
